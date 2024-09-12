34cd4f06130b87f79ecdd263e73fc423b22234fe
Joaquin Barrientos <joaquin2899@gmail.com>
2024-09-11 22:29:35 -0600

    Merge snarkjs-implementation into qr-scanner-button-implementation

--- apps/jubmoji-quest/src/lib/config.ts
+++ apps/jubmoji-quest/src/lib/config.ts
@@ -9,3 +9,13 @@ export const getClientPathToCircuits = (): string => {
 export const getServerPathToCircuits = (): string => {
   return path.resolve(process.cwd(), "public", "circuits") + "/";
 };
+
+// In our current configuration, this is the path to the circuits directory for client side proving
+export const getClientPathToLocationCircuits = (): string => {
+  return __dirname + "circuits/location_circuits";
+};
+
+// In our current configuration, this is the path to the circuits directory for server side proving
+export const getServerPathToLocationCircuits = (): string => {
+  return path.resolve(process.cwd(), "public", "circuits", "location_circuits") + "/";
+};
